1e87fd6287d3cc8e3f39dda3c4bbdf46d8af19fe
ensarkunduraci <tr.ensar.fi@gmail.com>
2025-02-11 15:30:16 +0200

    Updated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import React from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import './App.css'
-import MapView from './pages/MapView.jsx'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import './App.css';
+import Home from './pages/Home.jsx';
+import MapView from './pages/MapView.jsx';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MapView />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element={<MapView />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
