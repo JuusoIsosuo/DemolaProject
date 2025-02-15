6570936f8b5cf0c343f1b8bff1e5e39fefa64075
Jani Sarja <jani.sarja@live.com>
2025-02-15 20:04:38 +0200

    Get routes in frontend

--- backend/index.js
+++ backend/index.js
@@ -15,32 +15,31 @@ app.use(cors())
 
 app.use(express.json())
 
-// Example Usage
-console.log("Singapore to New York");
-const routes1 = findBestRoutes("Singapore", "New York", [103.8198, 1.3521], [-74.006, 40.7128])
-console.log(util.inspect(routes1, false, null, true));
-
-console.log("Singapore to Tampere");
-const routes2 = findBestRoutes("Singapore", "Tampere", [103.8198, 1.3521], [23.78712, 61.49911])
-console.log(util.inspect(routes2, false, null, true));
-
-console.log("Paris to Tokyo");
-const routes3 = findBestRoutes("Paris", "Tokyo", [2.3522, 48.8566], [139.6917, 35.6895])
-console.log(util.inspect(routes3, false, null, true));
-
 app.get("/routes", (req, res) => {
-  const geojsonPath = path.join(__dirname, "data", "routes.geojson")
+  try {
+    const origin = req.query.origin
+    const destination = req.query.destination
+    const originCoordinates = req.query.originCoords;
+    const destinationCoordinates = req.query.destCoords;
 
-  console.log(geojsonPath)
-
-  fs.readFile(geojsonPath, "utf8", (err, data) => {
-    console.log(data)
-    if (err) {
-        return res.status(500).json({ error: "Failed to read GeoJSON file" })
+    if (!originCoordinates || !destinationCoordinates) {
+      return res.status(400).json({ error: "Missing origin or destination coordinates" })
     }
 
-    res.json(JSON.parse(data))
-  })
+    const originCoordinatesArray = originCoordinates.split(",").map(Number)
+    const destinationCoordinatesArray = destinationCoordinates.split(",").map(Number)
+
+    if (originCoordinatesArray.length !== 2 || destinationCoordinatesArray.length !== 2) {
+      return res.status(400).json({ error: "Invalid origin or destination coordinates" })
+    }
+
+    const routes = findBestRoutes(origin, destination, originCoordinatesArray, destinationCoordinatesArray);
+
+    res.json(routes);
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: "Failed to find routes" })
+  }
 })
 
 app.get("/searoutes", async (req, res) => {
